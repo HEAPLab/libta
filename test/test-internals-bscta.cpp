6175043960e78b231586bc44598d8b6e111e68fd
Federico Reghenzani <federico.reghenzani@polimi.it>
2020-03-13 11:28:17 +0100

    [Testing] Convert bscta testing to googletest framework
    Signed-off-by: Federico Reghenzani <federico.reghenzani@polimi.it>

--- test/test-internals-bscta.cpp
+++ test/test-internals-bscta.cpp
@@ -1,48 +1,48 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-#include "libta/libta.h"
+#include "gtest/gtest.h"
 
-TEST_CASE( "Request tests", "[Request]" ) {
-    libta::Request<int> req;
-    req.add_value(1);
-    REQUIRE( *req.begin() == 1 );
+#include "bscta/libta_math.h"
+
+#include <iostream>
+#include <random>
+
+#define GTEST_COUT std::cerr << "[          ] "
+
+
+TEST(internal_test, test_sqrt_int)
+{
+    EXPECT_EQ(libta::internalSqrt(4),  2);
+    EXPECT_EQ(libta::internalSqrt(16), 4);
+    EXPECT_EQ(libta::internalSqrt(64), 8);
 }
 
-TEST_CASE( "Sqrt integer" ) {
-    REQUIRE( libta::internalSqrt(4) == 2 );
-    REQUIRE( libta::internalSqrt(16) == 4 );
-    REQUIRE( libta::internalSqrt(64) == 8 );
+TEST(internal_test, test_sqrt_long_double)
+{
+	EXPECT_LE( libta::internalSqrt((long double)3) , 1.74 );
+	EXPECT_GE( libta::internalSqrt((long double)3) , 1.72 );
+	EXPECT_LE( libta::internalSqrt((long double)10), 3.17 );
+	EXPECT_GE( libta::internalSqrt((long double)10), 3.16 );
+	EXPECT_LE( libta::internalSqrt((long double)70), 8.37 );
+	EXPECT_GE( libta::internalSqrt((long double)70), 8.36 );
 }
 
-
-TEST_CASE( "Sqrt long double" ) {
-	REQUIRE( libta::internalSqrt((long double)3) <= 1.74 );
-	REQUIRE( libta::internalSqrt((long double)3) >= 1.72 );
-	REQUIRE( libta::internalSqrt((long double)10) <= 3.17 );
-	REQUIRE( libta::internalSqrt((long double)10) >= 3.16 );
-	REQUIRE( libta::internalSqrt((long double)70) <= 8.37 );
-	REQUIRE( libta::internalSqrt((long double)70) >= 8.36 );
-
-}
-
-
-TEST_CASE( "getUnbiasedMean" ) {
+TEST(internal_test, test_getUnbiasedMean)
+{
 	std::vector<double> tmp;
 	for(int i=0;i<6;i++)tmp.push_back((double)(i+1));
-	REQUIRE( libta::getUnbiasedMean(tmp, 0, 6) == -2.5 );	
+	EXPECT_EQ( libta::getUnbiasedMean(tmp, 0, 6), -2.5 );
 	tmp.clear();
 	for(int i=10 ; i>0 ; i-- ) tmp.push_back(i);
-	REQUIRE( libta::getUnbiasedMean(tmp, 0, 10) == 4.5 );	
-
+	EXPECT_EQ( libta::getUnbiasedMean(tmp, 0, 10), 4.5 );
 }
 
-TEST_CASE("setExponProbabilyDensityFunction"){
+TEST(internal_test, test_setExponProbabilyDensityFunction)
+{
 	std::vector<double> tmp;
 	for(int i=0;i<10;i++)tmp.push_back((double)(i));
 	double sum = libta::setExponProbabilyDensityFunction(tmp , tmp,(double)1);
 	int left = sum > 1.5819000 ;
-	int right = sum < 1.5820000 ;	
-	REQUIRE( (left * right) == 1 );	
+	int right = sum < 1.5820000 ;
+	EXPECT_EQ( (left * right), 1 );
 	tmp.clear();
 	double var = 0.5;
 	for(int i=0;i<20;i++){
@@ -52,7 +52,7 @@ TEST_CASE("setExponProbabilyDensityFunction"){
 	sum = libta::setExponProbabilyDensityFunction(tmp , tmp,(double)1);
 	left = sum > 0.959500 ;
 	right = sum < 0.959600 ;
-	REQUIRE( (left * right) == 1 );	
+	EXPECT_EQ( (left * right), 1 );
 	tmp.clear();
 
 	var = 0.5;
@@ -63,10 +63,11 @@ TEST_CASE("setExponProbabilyDensityFunction"){
 	sum = libta::setExponProbabilyDensityFunction(tmp , tmp,(double)1.5);
 	left = sum > 0.912057 ;
 	right = sum < 0.912058 ;
-	REQUIRE( (left * right) == 1 );	
+	EXPECT_EQ( (left * right), 1 );
 }
 
-TEST_CASE("setExponSurvivalFunction"){
+TEST(internal_test, test_setExponSurvivalFunction)
+{
 	std::vector<double> tmp;
 	double var = 0.5;
 	for(int i=0;i<10;i++){
@@ -78,11 +79,10 @@ TEST_CASE("setExponSurvivalFunction"){
 	for(int i=0;i<10;i++) sum_surv+=tmp[i];
 	int left  = sum_surv >= 1.2872;
 	int right = sum_surv <= 1.2873;
-	REQUIRE( (left * right) == 1 );	
-
+	EXPECT_EQ( (left * right), 1 );
 
 	tmp.clear();
-	var = 1.75 ; 
+	var = 1.75 ;
 	for(int i=0;i<20;i++){
 		tmp.push_back(var);
 		var+=1.0505;
@@ -92,5 +92,8 @@ TEST_CASE("setExponSurvivalFunction"){
 	for(int i=0;i<20;i++) sum_surv+=tmp[i];
 	left = sum_surv >= 1.26070;
 	right = sum_surv <= 1.26080;
-	REQUIRE( (left * right) == 1 );	
+	EXPECT_EQ( (left * right), 1 );
 }
+
+
+
--- tests/src/tests-main.cpp
+++
@@ -1,6 +0,0 @@
-
-//This is the main
-// tests-main.cpp
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-//#include "catch.hpp"
